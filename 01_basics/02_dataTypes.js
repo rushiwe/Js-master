0f20ad07bbbbfe9140f541222a56f2409e12decd
rushiwe <105355084+rushiwe@users.noreply.github.com>
2024-10-08 14:46:34 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "Rushikesh"
+let age = "18"
+let isLoggedIn = "false"
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standalone value
+// undefind => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
